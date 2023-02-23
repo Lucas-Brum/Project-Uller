16e6aab1a58ab991497636463e386495ed1897ce
Lucas Brum <lucasmatheusrodriguesbrum@gmail.com>
2023-02-23 16:10:33 -0300

    Refactory the code

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Project Uller is a Python-based scrapper that aims to collect images from variou
 1. ~~Connect to at one web site~~
 2. ~~Connect to a list of websites~~
 3. ~~Download images from these pages with generic code~~
-4. ...
+4. ~~Refactory the code~~
+5. Connect to database
--- uller.py
+++ uller.py
@@ -2,42 +2,56 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-url_list = ['https://portoalegre-airport.com.br/pt/',
-            'https://www.ahnegao.com.br/', 'https://openai.com/blog/chatgpt/']
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.104 Safari/537.3"
-}
-count = 0
 
-for url in url_list:
-
+def get_html(url, headers):
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
+    return response.content if response.status_code == 200 else None
+
+
+def get_image_url(image):
+    img_url = image.get("src")
+    if not img_url:
+        img_url = image.get("class")
+    return img_url
+
+
+def download_image(url, img_url, count, headers):
+    if 'https' in img_url:
+        response = requests.get(img_url, headers=headers)
+        open(f"{url.split('/')[-2]}/{count}.jpg", "wb").write(response.content)
+    else:
+        response = requests.get(f"{url}{img_url}", headers=headers)
+        open(f"{url.split('/')[-2]}/{count}.jpg", "wb").write(response.content)
+
+
+def download_images(url_list):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.104 Safari/537.3"
+    }
+    count = 0
+    for url in url_list:
+        html = get_html(url, headers)
+        if not html:
+            continue
+
+        soup = BeautifulSoup(html, "html.parser")
         images = soup.find_all("img")
 
-        if not os.path.exists("images"):
-            os.makedirs("images")
+        if not os.path.exists(url.split('/')[-2]):
+            os.makedirs(url.split('/')[-2])
 
-        for i, image in enumerate(images):
+        for image in images:
             try:
-                img_url = image["src"]
-                if not img_url:
-                    # ou qualquer outro atributo que possa ser usado para identificar a URL da imagem
-                    img_url = image["class"]
-                if 'https' in img_url:
-                    response = requests.get(img_url, headers=headers)
-                    open(f"images/{count}.jpg", "wb").write(response.content)
-                else:
-                    response = requests.get(url+img_url, headers=headers)
-                    open(f"images/{count}.jpg", "wb").write(response.content)
+                img_url = get_image_url(image)
+                download_image(url, img_url, count, headers)
             except (KeyError, requests.exceptions.RequestException) as e:
-                if not img_url:
-                    img_url = image["class"]
-                if 'https' in img_url:
-                    response = requests.get(img_url, headers=headers)
-                    open(f"images/{count}.jpg", "wb").write(response.content)
-                else:
-                    response = requests.get(url+img_url, headers=headers)
-                    open(f"images/{count}.jpg", "wb").write(response.content)
-            count = count + 1
+                img_url = get_image_url(image)
+                download_image(url, img_url, count, headers)
+
+            count += 1
+
+
+if __name__ == "__main__":
+    url_list = ['https://portoalegre-airport.com.br/pt/',
+                'https://www.ahnegao.com.br/', 'https://openai.com/blog/chatgpt/']
+    download_images(url_list)
